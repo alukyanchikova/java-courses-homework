be59fb8342168364e50db7a4d25d0a06fff030fe
alukyanchikova <alexandralukyanchikova@gmail.com>
2017-01-03 12:56:49 +0300

    move login into BeforeMethod

--- addressbook-web-tests/src/test/java/ru/stqa/jchw/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jchw/addressbook/GroupCreationTests.java
@@ -18,10 +18,6 @@ public class GroupCreationTests {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\Alexander_Lukyanchik\\www\\geckodriver.exe");
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests() {
         wd.get("http://localhost:41062/www/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -30,6 +26,11 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation() {
+
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
