7326f484a90838e323a5dc70f6617ef142beed7b
alukyanchikova <alukyanchikova@at-consulting.ru>
2017-01-14 19:54:12 +0300

    add ability to choose a browser to run

--- addressbook-web-tests/src/test/java/ru/stqa/jchw/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jchw/addressbook/appmanager/ApplicationManager.java
@@ -1,19 +1,42 @@
 package ru.stqa.jchw.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
+import static org.openqa.selenium.remote.BrowserType.*;
+
 public class ApplicationManager {
-    private FirefoxDriver wd;
+    private WebDriver wd;
+
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Alexander_Lukyanchik\\www\\geckodriver.exe");
-        wd = new FirefoxDriver();
+        //System.setProperty("webdriver.gecko.driver", "C:\\Users\\Alexander_Lukyanchik\\www\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\ALukyanchikova\\Desktop\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ALukyanchikova\\Desktop\\chromedriver.exe");
+        System.setProperty("webdriver.ie.driver", "C:\\Users\\ALukyanchikova\\Desktop\\IEDriverServer.exe");
+
+        if (browser.equals(FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(IE)) {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost:41062/www/addressbook/group.php");
         groupHelper = new GroupHelper(wd);
