67694adf88f3efb0894cc0011faa4578402d68b6
alukyanchikova <alukyanchikova@at-consulting.ru>
2017-02-05 16:23:25 +0300

    fluent-interface using decorator pattern ang guava added

--- addressbook-web-tests/src/test/java/ru/stqa/jchw/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jchw/addressbook/appmanager/GroupHelper.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.jchw.addressbook.model.GroupData;
+import ru.stqa.jchw.addressbook.model.Groups;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class GroupHelper extends HelperBase {
@@ -36,8 +36,8 @@ public class GroupHelper extends HelperBase {
         clickOnForm(By.name("delete"));
     }
 
-    public void selectGroup(int index) {
-        wd.findElements(By.name("selected[]")).get(index).click();
+    public void selectGroupById(int id) {
+        wd.findElement(By.cssSelector("input[value ='" + id + "']")).click();
     }
 
     public void initGroupModification() {
@@ -55,16 +55,16 @@ public class GroupHelper extends HelperBase {
         returnToGroupPage();
     }
 
-    public void modify(int indexGroupForModification, GroupData group) {
-        selectGroup(indexGroupForModification);
+    public void modify(GroupData group) {
+        selectGroupById(group.getId());
         initGroupModification();
         fillGroupForm(group);
         submitGroupModification();
         returnToGroupPage();
     }
 
-    public void delete(int indexGroupDelition) {
-        selectGroup(indexGroupDelition);
+    public void delete(GroupData group) {
+        selectGroupById(group.getId());
         deleteSelectedGroup();
         returnToGroupPage();
     }
@@ -77,8 +77,8 @@ public class GroupHelper extends HelperBase {
         return wd.findElements(By.name("selected[]")).size();
     }
 
-    public List<GroupData> list() {
-        List<GroupData> groups = new ArrayList<GroupData>();
+    public Groups all() {
+        Groups groups = new Groups();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements) {
             String name = element.getText();
